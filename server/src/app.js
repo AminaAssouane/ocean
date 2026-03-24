daf5bfb38448ea0b960a094e13e0df1798ed440f
Amina <minassouane@gmail.com>
2026-03-24 23:00:32 +0100

    feat(server): add cors and body parsing middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
+require("dotenv/config");
+const cors = require("cors");
 const express = require("express");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
