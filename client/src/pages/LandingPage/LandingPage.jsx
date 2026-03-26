9db132fcd9480ff3f3c5b2674b4f1f265c6c0ba4
Amina <minassouane@gmail.com>
2026-03-26 22:33:37 +0100

    style(auth): style the landing page

---
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import styles from "./LandingPage.module.css";
+import landingPic from "../../assets/images/LandingPic.png";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+export default function LandingPage() {
+  const [view, setView] = useState("login");
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.authSection}>
+        <h1 className={styles.title}>Ocean</h1>
+
+        <div className={styles.form}>
+          <p>Dive in</p>
+          <p>Please fill in your details below</p>
+          {view === "login" ? (
+            <LoginForm onSwitch={() => setView("register")} />
+          ) : (
+            <RegisterForm onSwitch={() => setView("login")} />
+          )}
+        </div>
+        <footer>
+          <div className={styles.credit}>© Made by Amina Assouane</div>
+          <div className={styles.links}>
+            <a href="https://github.com/AminaAssouane" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/amina-assouane/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </footer>
+      </section>
+      <section className={styles.imageSection}>
+        <img src={landingPic} alt="" className={styles.landingImage} />
+      </section>
+    </main>
+  );
+}
