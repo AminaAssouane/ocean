97eb6bfcb0d0f79ecbae6c1f3fc575e7479cc466
Amina <minassouane@gmail.com>
2026-03-26 22:14:50 +0100

    refactor(main): delete login and register pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,9 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 
-const router = createBrowserRouter([
-  { path: "/register", element: <Register /> },
-  { path: "/login", element: <Login /> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <LandingPage /> }]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
