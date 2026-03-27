7241a0d70b6d21039b9347be4bb7f80be35ad773
Amina <minassouane@gmail.com>
2026-03-27 06:08:26 +0100

    feat(layout): add header component and delete custom cursors

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import oceanIcon from "../../assets/icons/ocean.svg";
+export default function Header() {
+  return (
+    <div>
+      <img src={oceanIcon} />
+      <h1>Ocean</h1>
+    </div>
+  );
+}
