a44b94becea66629943781d6a030f71692f45661
Amina <minassouane@gmail.com>
2026-03-27 03:12:26 +0100

    feat(layout): add dashboardlayout component logic

--- client/src/pages/DashboardLayout/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout/DashboardLayout.jsx
@@ -1,1 +1,13 @@
-export default function DashboardLayout() {}
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+
+export default function DashboardLayout() {
+  return (
+    <div>
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
