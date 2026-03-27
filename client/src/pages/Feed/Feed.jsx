268ac0200707925307eb0eff416dbadafb6ba39b
Amina <minassouane@gmail.com>
2026-03-27 18:12:40 +0100

    feat(posts): add create post in feed

--- client/src/pages/Feed/Feed.jsx
+++ client/src/pages/Feed/Feed.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import api from "../../services/api";
+
 export default function Feed() {
-  return <h1>Feed</h1>;
+  const [content, setContent] = useState("");
+  async function handlePost() {
+    try {
+      if (!content.trim()) return;
+      await api.post("/dashboard/createPost", { content });
+      console.log("Posting:", content);
+      setContent("");
+    } catch (error) {
+      console.log("Failed to create post ", error);
+    }
+  }
+  return (
+    <>
+      <h1>Feed</h1>
+      <textarea
+        name="content"
+        id=""
+        placeholder="What's happening?"
+        required
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
+      <button onClick={handlePost}>Post</button>
+    </>
+  );
 }
