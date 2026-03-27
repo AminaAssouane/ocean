7af6e6545b80c592c3600d3bf9f70405ec642567
Amina <minassouane@gmail.com>
2026-03-27 12:14:28 +0100

    style(sidebar): add styling to the sidebar component

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,66 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Sidebar.module.css";
+import {
+  House,
+  CircleUser,
+  SquarePlus,
+  Users,
+  Heart,
+  LogOut,
+} from "lucide-react";
 
 export default function Sidebar() {
   return (
-    <aside>
-      <NavLink to="/dashboard">Home</NavLink>
-      <NavLink to="/dashboard/profile">Profile</NavLink>
-      <NavLink to="/dashboard/create">Create</NavLink>
-      <NavLink to="/dashboard/users">Users</NavLink>
-      <NavLink to="/dashboard/liked">Likes</NavLink>
-      <NavLink>Logout</NavLink>
+    <aside className={styles.sideBarContainer}>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <House />
+        Home
+      </NavLink>
+      <NavLink
+        to="/dashboard/profile"
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <CircleUser /> Profile
+      </NavLink>
+      <NavLink
+        to="/dashboard/create"
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <SquarePlus /> Create
+      </NavLink>
+      <NavLink
+        to="/dashboard/users"
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <Users /> Users
+      </NavLink>
+      <NavLink
+        to="/dashboard/liked"
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <Heart /> Likes
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? styles.active : styles.navLink
+        }
+      >
+        <LogOut />
+        Logout
+      </NavLink>
     </aside>
   );
 }
