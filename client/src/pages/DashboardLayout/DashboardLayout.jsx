9bb70bf5d23b29da1440a98f367ac3acb860e11d
Amina <minassouane@gmail.com>
2026-03-27 06:49:18 +0100

    style(layout): add styling to the dashboard

--- client/src/pages/DashboardLayout/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout/DashboardLayout.jsx
@@ -1,13 +1,18 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Header from "../../components/Header/Header";
+import styles from "./DashboardLayout.module.css";
 import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
-    <div>
-      <Header />
-      <Sidebar />
-      <main>
+    <div className={styles.dashboardLayout}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
+      <main className={styles.main}>
         <Outlet />
       </main>
     </div>
