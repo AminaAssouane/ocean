e8e3f4815b5ca5cdc304b24cca80bc69866740df
Amina <minassouane@gmail.com>
2026-03-28 15:42:39 +0100

    feat(posts): add create post page

--- client/src/pages/CreatePost/CreatePost.jsx
+++ client/src/pages/CreatePost/CreatePost.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import api from "../../services/api";
+
 export default function CreatePost() {
-  return <div></div>;
+  const [content, setContent] = useState("");
+
+  async function handlePost() {
+    try {
+      if (!content.trim()) return;
+      await api.post("/dashboard/createPost", { content });
+      console.log("Posting:", content);
+      setContent("");
+    } catch (error) {
+      console.log("Failed to create post. ", error);
+    }
+  }
+  return (
+    <section>
+      <textarea
+        name="content"
+        id=""
+        placeholder="What's happening?"
+        required
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
+      <button onClick={handlePost}>
+        <SendHorizontal />
+      </button>
+    </section>
+  );
 }
