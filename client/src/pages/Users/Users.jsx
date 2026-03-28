aff5bdec234f08df8f185c59207f03c06877548b
Amina <minassouane@gmail.com>
2026-03-28 23:42:09 +0100

    feat(users): add search users

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
+import api from "../../services/api";
+import styles from "./Users.module.css";
 
 export default function Users() {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
-  function handleSearch() {}
+  async function handleSearch(e) {
+    try {
+      setQuery(e.target.value);
+      const res = await api.get(`/users/search?username=${query}`);
+      setResults(res.data);
+    } catch (error) {
+      console.error("Failed to search for users. ", error);
+    }
+  }
 
   return (
     <div>
-      <input type="text" value={query} onChange={handleSearch} />
-      <button>Search</button>
+      <section className={styles.searchSection}>
+        <input type="text" value={query} onChange={handleSearch} />
+        <button>Search</button>
+      </section>
+      <section className={styles.userList}></section>
     </div>
   );
 }
