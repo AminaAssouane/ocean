3465ebe2e44af02418f267c5a0bf9dbe3ae29c68
Amina <minassouane@gmail.com>
2026-03-28 18:37:50 +0100

    feat(users): implement fetch user by id logic

--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -1,1 +1,28 @@
-export default function UserProfile() {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
+export default function UserProfile() {
+  const { userId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getUserById() {
+      try {
+        const res = await api.get(`/users/${userId}`);
+        setUser(res.data);
+      } catch (error) {
+        console.error("Failed to fetch user by id. ", error);
+      }
+    }
+    getUserById();
+  }, []);
+
+  if (!user) return <p>Loading...</p>;
+
+  return (
+    <section>
+      <div>{user.username}</div>
+    </section>
+  );
+}
