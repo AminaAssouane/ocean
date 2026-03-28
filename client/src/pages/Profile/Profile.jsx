d305173d1ce804d1b031ff8109061cd4e87539b6
Amina <minassouane@gmail.com>
2026-03-28 17:43:34 +0100

    feat(profile): implement profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,1 +1,26 @@
-export default function Profile() {}
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+export default function Profile() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getSelf() {
+      try {
+        const res = await api.get("/users");
+        setUser(res.data);
+      } catch (error) {
+        console.error("Failed to fetch user. ", error);
+      }
+    }
+    getSelf();
+  }, []);
+
+  if (!user) return <p>Loading...</p>;
+
+  return (
+    <section>
+      <div>{user.username}</div>
+    </section>
+  );
+}
