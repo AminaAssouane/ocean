42061776a6ba4069672dd6b0b752cd90b7cecec4
Amina <minassouane@gmail.com>
2026-03-29 17:34:40 +0100

    feat(likes): add like button to post preview

--- client/src/components/PostPreview/PostPreview.jsx
+++ client/src/components/PostPreview/PostPreview.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import styles from "./PostPreview.module.css";
+import LikeButton from "../LikeButton/LikeButton";
+import api from "../../services/api";
 
 export default function PostPreview({ post }) {
+  const [isLiked, setIsLiked] = useState(false);
   const navigate = useNavigate();
   function handleClick() {
     navigate(`/dashboard/post/${post.id}`);
   }
   const authorId = post.authorId;
+
+  useEffect(() => {
+    async function isLiked() {
+      try {
+        const liked = await api.get(`/likes/${post.id}`);
+        setIsLiked(liked);
+      } catch (error) {
+        console.error("Failed to fetch like status of the post. ", error);
+      }
+    }
+    isLiked();
+  }, []);
+
   return (
     <article onClick={handleClick} className={styles.postPreviewContainer}>
       <div className={styles.username}>
@@ -15,6 +32,7 @@ export default function PostPreview({ post }) {
         </Link>
       </div>
       <div className={styles.content}>{post.content}</div>
+      <LikeButton postId={post.id} initialState={isLiked} />
     </article>
   );
 }
