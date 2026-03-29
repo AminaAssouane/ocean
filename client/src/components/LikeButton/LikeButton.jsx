a1baf3fdb73b8912e3c876985a1202cc90b5b8e5
Amina <minassouane@gmail.com>
2026-03-29 21:44:46 +0100

    fix(likes): move likebutton state from postpreview to likebutton

--- client/src/components/LikeButton/LikeButton.jsx
+++ client/src/components/LikeButton/LikeButton.jsx
@@ -1,21 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import api from "../../services/api";
 
-export default function LikeButton({ postId, initialState = false }) {
-  const [liked, setLiked] = useState(initialState);
+export default function LikeButton({ postId }) {
+  const [liked, setLiked] = useState(false);
+
   async function handleClick(e) {
     e.stopPropagation();
     try {
       if (liked) {
-        await api.delete(`likes/${postId}`);
+        await api.delete(`/likes/${postId}`);
       } else {
-        await api.post(`likes/${postId}`);
+        const what = await api.post(`/likes/${postId}`);
+        console.log(what);
       }
       setLiked(!liked);
     } catch (error) {
       console.error("Failed to like/dislike post. ", error);
     }
   }
+
+  useEffect(() => {
+    async function isLiked() {
+      try {
+        const res = await api.get(`/likes/${postId}`);
+        setLiked(res.data);
+      } catch (error) {
+        console.error("Failed to fetch like status of the post. ", error);
+      }
+    }
+    isLiked();
+  }, []);
 
   return <button onClick={handleClick}>{liked ? "dislike" : "like"}</button>;
 }
