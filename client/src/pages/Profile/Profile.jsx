b663cb5591b00ae72655d2c4be83432fe635d1c4
Amina <minassouane@gmail.com>
2026-03-30 19:42:36 +0100

    style (profile): add styling to users' profiles

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import PostPreview from "../../components/PostPreview/PostPreview";
 import api from "../../services/api";
 import styles from "./Profile.module.css";
 import avatar from "../../assets/images/ProfilePic.png";
@@ -8,6 +9,7 @@ export default function Profile() {
   const [user, setUser] = useState(null);
   const [following, setFollowing] = useState(null);
   const [followers, setFollowers] = useState(null);
+  const [posts, setPosts] = useState(null);
 
   useEffect(() => {
     async function getProfileData() {
@@ -22,6 +24,9 @@ export default function Profile() {
 
         const followersRes = await api.get(`/followers/${res.data.id}`);
         setFollowers(followersRes.data.count);
+
+        const postsRes = await api.get(`/dashboard/user/${res.data.id}`);
+        setPosts(postsRes.data);
       } catch (error) {
         console.error("Failed to fetch profile data. ", error);
       }
@@ -29,31 +34,40 @@ export default function Profile() {
     getProfileData();
   }, []);
 
-  if (!user || following === null || followers === null)
+  if (!user || following === null || followers === null || posts === null)
     return <p>Loading...</p>;
 
   return (
     <section>
-      <div>
-        <div className={styles.coverContainer}>
-          <img src={cover} alt="" className={styles.cover} />
-          <img src={avatar} alt="" className={styles.avatar} />
+      <div className={styles.coverContainer}>
+        <img src={cover} alt="" className={styles.cover} />
+        <img src={avatar} alt="" className={styles.avatar} />
+      </div>
+      <div className={styles.infoContainer}>
+        <div className={styles.username}>{user.username}</div>
+        <div className={styles.date}>
+          Joined{" "}
+          {new Date(user.createdAt).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
         </div>
-        <div className={styles.infoContainer}>
-          <div className={styles.username}>{user.username}</div>
-          <div className={styles.date}>
-            Joined{" "}
-            {new Date(user.createdAt).toLocaleDateString("en-GB", {
-              day: "numeric",
-              month: "long",
-              year: "numeric",
-            })}
+        <div className={styles.followInfo}>
+          <div className={styles.following}>
+            <span className={styles.followNumber}>{following}</span>{" "}
+            <span className={styles.follow}>Following</span>
           </div>
-          <div className={styles.followInfo}>
-            <div className={styles.following}>{following} Following</div>
-            <div className={styles.followers}>{followers} Followers</div>
+          <div className={styles.followers}>
+            <span className={styles.followNumber}>{followers}</span>{" "}
+            <span className={styles.follow}>Followers</span>
           </div>
         </div>
+      </div>
+      <div className={styles.postsContainer}>
+        {posts.map((post) => (
+          <PostPreview key={post.id} post={post} />
+        ))}
       </div>
     </section>
   );
--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -1,23 +1,39 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
+import PostPreview from "../../components/PostPreview/PostPreview";
 import FollowButton from "../../components/FollowButton/FollowButton";
+import styles from "./UserProfile.module.css";
+import avatar from "../../assets/images/ProfilePic.png";
+import cover from "../../assets/images/ocean-cover.jpg";
 
 export default function UserProfile() {
   const { userId } = useParams();
   const [user, setUser] = useState(null);
   const [followed, setFollowed] = useState(null);
+  const [following, setFollowing] = useState(null);
+  const [followers, setFollowers] = useState(null);
+  const [posts, setPosts] = useState(null);
 
   useEffect(() => {
-    async function getUserById() {
+    async function getUserData() {
       try {
         const res = await api.get(`/users/${userId}`);
         setUser(res.data);
+
+        const followingRes = await api.get(`/followers/${userId}/following`);
+        setFollowing(followingRes.data.count);
+
+        const followersRes = await api.get(`/followers/${userId}`);
+        setFollowers(followersRes.data.count);
+
+        const postsRes = await api.get(`/dashboard/user/${userId}`);
+        setPosts(postsRes.data);
       } catch (error) {
-        console.error("Failed to fetch user by id. ", error);
+        console.error("Failed to fetch profile data. ", error);
       }
     }
-    getUserById();
+    getUserData();
   }, []);
 
   useEffect(() => {
@@ -32,14 +48,47 @@ export default function UserProfile() {
     isFollowed();
   }, []);
 
-  if (!user) return <p>Loading user...</p>;
-  if (!followed) return <p>Loading follow...</p>;
+  if (
+    !user ||
+    !followed ||
+    following === null ||
+    followers === null ||
+    posts === null
+  )
+    return <p>Loading...</p>;
 
   return (
     <section>
-      <div>
-        {user.username}{" "}
+      <div className={styles.coverContainer}>
+        <img src={cover} alt="" className={styles.cover} />
+        <img src={avatar} alt="" className={styles.avatar} />
+      </div>
+      <div className={styles.infoContainer}>
+        <div className={styles.username}>{user.username}</div>
         <FollowButton userId={userId} initialFollowing={followed} />
+        <div className={styles.date}>
+          Joined{" "}
+          {new Date(user.createdAt).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </div>
+        <div className={styles.followInfo}>
+          <div className={styles.following}>
+            <span className={styles.followNumber}>{following}</span>{" "}
+            <span className={styles.follow}>Following</span>
+          </div>
+          <div className={styles.followers}>
+            <span className={styles.followNumber}>{followers}</span>{" "}
+            <span className={styles.follow}>Followers</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.postsContainer}>
+        {posts.map((post) => (
+          <PostPreview key={post.id} post={post} />
+        ))}
       </div>
     </section>
   );
