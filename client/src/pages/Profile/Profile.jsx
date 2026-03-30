8f765e797ca8599d3f929d035e20a84b5ec86104
Amina <minassouane@gmail.com>
2026-03-30 17:42:24 +0100

    style(profile): fetch and display number of followers and followings

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -6,20 +6,31 @@ import cover from "../../assets/images/ocean-cover.jpg";
 
 export default function Profile() {
   const [user, setUser] = useState(null);
+  const [following, setFollowing] = useState(null);
+  const [followers, setFollowers] = useState(null);
 
   useEffect(() => {
-    async function getSelf() {
+    async function getProfileData() {
       try {
         const res = await api.get("/users");
         setUser(res.data);
+
+        const followingRes = await api.get(
+          `/followers/${res.data.id}/following`,
+        );
+        setFollowing(followingRes.data.count);
+
+        const followersRes = await api.get(`/followers/${res.data.id}`);
+        setFollowers(followersRes.data.count);
       } catch (error) {
-        console.error("Failed to fetch user. ", error);
+        console.error("Failed to fetch profile data. ", error);
       }
     }
-    getSelf();
+    getProfileData();
   }, []);
 
-  if (!user) return <p>Loading...</p>;
+  if (!user || following === null || followers === null)
+    return <p>Loading...</p>;
 
   return (
     <section>
@@ -37,6 +48,10 @@ export default function Profile() {
               month: "long",
               year: "numeric",
             })}
+          </div>
+          <div className={styles.followInfo}>
+            <div className={styles.following}>{following} Following</div>
+            <div className={styles.followers}>{followers} Followers</div>
           </div>
         </div>
       </div>
