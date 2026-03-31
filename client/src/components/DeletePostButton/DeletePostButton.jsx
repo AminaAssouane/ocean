f729e686301145772f087c540e6a125b0b64c7d4
Amina <minassouane@gmail.com>
2026-03-31 01:48:18 +0100

    feat(posts): add delete post button

---
+++ client/src/components/DeletePostButton/DeletePostButton.jsx
@@ -0,0 +1,15 @@
+import { Trash } from "lucide-react";
+import api from "../../services/api";
+
+export default function DeletePostButton({ postId }) {
+  async function handleClick(e) {
+    e.stopPropagation();
+    try {
+      await api.delete(`/dashboard/post/${postId}`);
+    } catch (error) {
+      console.error("Failed deleting post. ", error);
+    }
+  }
+
+  return <Trash onClick={handleClick} />;
+}
