26519727c4b5d37eb257910feffb6ac4dad4e3b8
Amina <minassouane@gmail.com>
2026-03-31 05:50:17 +0100

    feat(comments): add fetch number of comments of a post

--- server/src/controllers/commentController.js
+++ server/src/controllers/commentController.js
@@ -1,5 +1,18 @@
 const prisma = require("../lib/prisma");
 
+async function getNbComments(req, res) {
+  const postId = parseInt(req.params.id);
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { postId },
+    });
+    res.json(comments.length);
+  } catch (error) {
+    console.error("Failed fetching comments. ", error);
+    res.status(500).json({ message: "Failed fetching comments." });
+  }
+}
+
 async function getComments(req, res) {
   const postId = parseInt(req.params.id);
   try {
@@ -30,4 +43,4 @@ async function createComment(req, res) {
   }
 }
 
-module.exports = { getComments, createComment };
+module.exports = { getNbComments, getComments, createComment };
--- server/src/routes/commentRouter.js
+++ server/src/routes/commentRouter.js
@@ -3,6 +3,7 @@ const commentRouter = Router();
 const commentController = require("../controllers/commentController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+commentRouter.get("/:id/nb", authMiddleware, commentController.getNbComments);
 commentRouter.get("/:id", authMiddleware, commentController.getComments);
 commentRouter.post("/:id", authMiddleware, commentController.createComment);
 
