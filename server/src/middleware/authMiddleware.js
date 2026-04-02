9c3fa014a2885215b37eb1abfa4c3cfe5c529840
Amina <minassouane@gmail.com>
2026-04-02 10:24:14 +0100

    feat(auth): return error instead of redirection when user is not authenticated

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ function authMiddleware(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/login");
+  res.status(401).json({ message: "Unauthorized" });
 }
 
 module.exports = authMiddleware;
